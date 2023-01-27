f4dc171ddae0983fc7788907cd4699af1511389b
Officialravi888 <youremail@yourdomain.com>
2023-01-27 12:37:30 +0530

    Execption.File and Java 8 Q/A

--- src/main/java/org/example/program/NullPointerExceptionExample.java
+++ src/main/java/org/example/program/NullPointerExceptionExample.java
@@ -1,12 +1,26 @@
 package org.example.program;
 
-public class NullPointerExceptionExample {
-    private static void printLength(String str) {
-        System.out.println(str.length());
-    }
-    public static void main(String args[]) {
-        String myString = null;
-        printLength(myString);
-    }
-}
+//public class NullPointerExceptionExample {
+//    private static void printLength(String str) {
+//        System.out.println(str.length());
+//    }
+//    public static void main(String args[]) {
+//        String myString = null;
+//        printLength(myString);
+//    }
+//}
 
+//public class NullPointerExceptionExample {
+//    private static void printLength(String str) {
+//        if (StringUtils.isNotEmpty(str)) {
+//            System.out.println(str.length());
+//        } else {
+//            System.out.println("Empty string");
+//        }
+//    }
+//
+//    public static void main(String args[]) {
+//        String myString = null;
+//        printLength(myString);
+//    }
+//}
