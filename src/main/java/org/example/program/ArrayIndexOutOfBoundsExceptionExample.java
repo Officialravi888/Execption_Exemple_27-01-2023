19864a7a6d7f5efe68ba32ee5100c927f98d994a
Officialravi888 <youremail@yourdomain.com>
2023-01-27 12:18:38 +0530

    Exemple

---
+++ src/main/java/org/example/program/ArrayIndexOutOfBoundsExceptionExample.java
@@ -0,0 +1,9 @@
+package org.example.program;
+
+public class ArrayIndexOutOfBoundsExceptionExample {
+    public static void main(String[] args) {
+        String[] arr = new String[10];
+        System.out.println(arr[10]);
+    }
+}
+
---
+++ src/main/java/org/example/program/NullPointerExceptionExample.java
@@ -0,0 +1,12 @@
+package org.example.program;
+
+public class NullPointerExceptionExample {
+    private static void printLength(String str) {
+        System.out.println(str.length());
+    }
+    public static void main(String args[]) {
+        String myString = null;
+        printLength(myString);
+    }
+}
+
